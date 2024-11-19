56bd3793063e2036b040c25725548e383b5e568c
NRKim93 <ning9303@naver.com>
2024-11-19 17:47:01 +0900

    명언 게시판 9단계 작성 완료.

--- app/src/main/java/workspace/wisesaying/WiseSayingAddList.java
+++ app/src/main/java/workspace/wisesaying/WiseSayingAddList.java
@@ -1,5 +1,10 @@
 package workspace.wisesaying;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import static workspace.wisesaying.WiseSayingMain.*;
 
 public class WiseSayingAddList {
@@ -13,6 +18,31 @@ public class WiseSayingAddList {
         id_list.add(id);
         authur_list.add(authur_v);
         wise_saying_list.add(wise_saying_v);
+
+        File json_f = new File("C:\\WorkSpace\\app\\src\\main\\java\\workspace\\db\\wiseSaying\\",id+".json");
+        File id_f = new File("C:\\WorkSpace\\app\\src\\main\\java\\workspace\\db\\wiseSaying\\","lastId"+".txt");
+
+        String json = String.format(
+                "{ \"id \": %d, \"content \": \"%s\",\"authur\": \"%s\"}", id,wise_saying_v,authur_v
+        );
+
+        try {
+            if (id_f.createNewFile()) {
+                BufferedWriter bw = new BufferedWriter(new FileWriter(id_f));
+                bw.write(Integer.toString(id));
+                bw.close();
+            }
+
+            if (json_f.createNewFile()) {
+                BufferedWriter bw = new BufferedWriter(new FileWriter(json_f));
+                //bw.write("id : " + id + "\n" + "content : " + wise_saying_v + "\n" + "author : " + authur_v);
+                bw.write(json);
+                bw.close();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         System.out.println(id + " 번 명언이 등록되었습니다.");
         id++;
