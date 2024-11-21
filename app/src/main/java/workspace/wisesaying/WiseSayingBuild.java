7809c8b5a040b4dbc6c1d89cbf2d88729a69dd19
NRKim93 <ning9303@naver.com>
2024-11-21 11:14:46 +0900

    ㅁㅁㅁ

---
+++ app/src/main/java/workspace/wisesaying/WiseSayingBuild.java
@@ -0,0 +1,34 @@
+package workspace.wisesaying;
+
+import static workspace.wisesaying.WiseSayingMain.authur_list;
+import static workspace.wisesaying.WiseSayingMain.id_list;
+import static workspace.wisesaying.WiseSayingMain.wise_saying_list;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class WiseSayingBuild {
+
+    public static void BuildList() throws Exception{
+        File f = new File("C:\\WorkSpace\\app\\src\\main\\java\\workspace\\db\\wiseSaying\\data.json");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+
+        try {
+            bw.write("[");
+            for(int i = 0; i < id_list.size(); i++) {
+                String json = String.format("\n\t{ \n\t \"id \": %d, \n\t \"content \": \"%s\",\n\t \"authur\": \"%s\"\n\t}", id_list.get(i),wise_saying_list.get(i),authur_list.get(i));
+                if (i == id_list.size()-1) {
+                    bw.write(json);
+                } else {
+                    bw.write(json+",");
+                }
+            }
+            bw.write("\n]");
+            bw.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("하하 잘못됐지롱~");
+        }
+    }
+    
+}
